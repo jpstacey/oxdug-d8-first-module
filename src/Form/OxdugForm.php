c646c5dac31901272a1e85d4549da1a85e118317
J-P Stacey <work@jpstacey.info>
2015-02-02 18:10:07 +0000

    Adding validateForm (optional) handler and fleshing out submitForm

--- src/Form/OxdugForm.php
+++ src/Form/OxdugForm.php
@@ -48,6 +48,25 @@ class OxdugForm extends FormBase {
   /**
    * {@inheritdoc}
    */
-  public function submitForm(array &$form, FormStateInterface $form_state) {
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    if ($form_state->getValue('who_are_you') == "J-P") {
+      $form_state->setErrorByName(
+        'who_are_you',
+        $this->t("Is that you, J-P? Have you finished your D8 demo yet? I don't think so! Get back to work!")
+      );
+    }
   }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    drupal_set_message(
+      $this->t(
+        'Hello, %who! Welcome to OxDUG.',
+        array('%who' => $form_state->getValue('who_are_you'))
+      )
+    );
+  }
+
 }
